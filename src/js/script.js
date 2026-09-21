28523f251c7cde00c420261d404b5a09a3925bce
Gusta <gustavix@proton.me>
2026-09-21 19:17:34 -0300

    revisão de alguns comentarios dentro do index para facilitar leitura

--- src/js/script.js
+++ src/js/script.js
@@ -8,8 +8,9 @@ function carregarComponente (idContainer, caminhoArquivo) {
         .catch(erro => console.error(`Erro ao carregar ${caminhoArquivo}:`, erro));
 }
 
-// chamada de função
-carregarComponente('header-container', 'components/header.html');
-carregarComponente('hero-container', 'components/hero.html');
-carregarComponente('highlights-container', 'components/highlights.html');
-carregarComponente('category-container', 'components/category.html');
+// chamada de função - EX:
+// carregarComponente('id-do-container', 'caminho/do/container.html')
+carregarComponente('header-container', 'components/homepage/header.html');
+carregarComponente('hero-container', 'components/homepage/hero.html');
+carregarComponente('highlights-container', 'components/homepage/highlights.html');
+carregarComponente('categories-container', 'components/homepage/categories.html');
